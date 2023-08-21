757936e4de07cfacdfc1c2ce316fbe780d3c8dce
José Marcel <josemarcel30@gmail.com>
2023-08-21 12:55:59 -0300

    Tipos de variáveis

--- Estudo-java-basico/src/TiposVariaveis.java
+++ Estudo-java-basico/src/TiposVariaveis.java
@@ -1,3 +1,18 @@
+/*   Tipo de variaveis 
+
+Tipos Primitivos:
+1.byte: Armazena valores inteiros de 8 bits. Faixa: -128 a 127.
+2.short: Armazena valores inteiros de 16 bits. Faixa: -32,768 a 32,767.
+3.int: Armazena valores inteiros de 32 bits. Faixa: -2^31 a 2^31 - 1.
+4.long: Armazena valores inteiros de 64 bits. Faixa: -2^63 a 2^63 - 1.
+5.float: Armazena números de ponto flutuante de 32 bits.
+6.double: Armazena números de ponto flutuante de 64 bits (maior precisão que o float).
+7.char: Armazena um único caractere Unicode de 16 bits.
+8.boolean: Armazena valores booleanos (true ou false) */
+
+
 public class TiposVariaveis {
-    
+ 
+
+
 }
