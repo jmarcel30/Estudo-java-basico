00b38239adcaee148406e410b6efcf12751124e2
José Marcel <josemarcel30@gmail.com>
2023-08-22 19:07:50 -0300

    Métodos em Java

---
+++ Estudo-java-basico/src/Metodos.java
@@ -0,0 +1,17 @@
+public class Metodos {
+  
+        public static int somar(int a, int b) {
+            int resultado = a + b;
+            return resultado;
+        }
+    
+        public static void main(String[] args) {
+            int x = 5;
+            int y = 10;
+            int resultadoSoma = somar(x, y);
+            System.out.println("A soma é: " + resultadoSoma);
+        }
+    }
+
+    
+ 
